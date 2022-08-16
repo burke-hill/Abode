237df7ef3dac7f0ca6334de24502f83a716b8be1
Burke Hill <burkehill@Burkes-MBP.lan>
2022-08-16 15:27:48 -0400

    added availability button functionality

--- Abode/app/src/main/java/com/example/abode/Availability.kt
+++ Abode/app/src/main/java/com/example/abode/Availability.kt
@@ -1,30 +1,16 @@
 package com.example.abode
 
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.webkit.WebView
 
-class MainActivity : AppCompatActivity() {
+class Availability : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_availability)
     }
-
-    fun availabilityLink(view: View) {
-
-        val intent = Intent(this, Availability::class.java)
-        startActivity(intent)
-
-
-    }
-
-
 }
